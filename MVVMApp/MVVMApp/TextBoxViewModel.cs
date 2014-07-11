0faf594cf685bd530498c50997d1ee0748466429
thinkAmi <dev.thinkami+git@gmail.com>
2014-07-12 05:42:32 +0900

    add 'Microsoft.TeamFoundation.MVVM.ViewModelBase + INotifyDataErrorInfo' sample

---
+++ MVVMApp/MVVMApp/TextBoxViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMApp
+{
+    class TextBoxViewModel : VMBase
+    {
+        private string _comment;
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                if (_comment == value) return;
+
+                _comment = value;
+
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    UpdateErrors(errorMessage: "入力必須です");
+                }
+                else
+                {
+                    UpdateErrors();
+                }
+
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
