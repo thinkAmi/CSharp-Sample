fcdea63360cb5f535b77513c0022a2ecf8203e48
thinkAmi <dev.thinkami+git@gmail.com>
2014-08-08 06:52:04 +0900

    add MessageBoxService sample

---
+++ MVVMApp/MessageBoxMVVM/DataContextChangedView.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MessageBoxMVVM
+{
+    /// <summary>
+    /// DataContextChangedView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DataContextChangedView : Window
+    {
+        public DataContextChangedView()
+        {
+            DataContextChanged += (sender, e) =>
+            {
+                var x = DataContext as DataContextChangedViewModel;
+                if (x == null) throw new InvalidCastException();
+
+                x.ShowInformationMessageBox = (message) => MessageBox.Show(message, "info", MessageBoxButton.OK, MessageBoxImage.Information);
+                x.ShowErrorMessageBox = (message) => MessageBox.Show(message, "error", MessageBoxButton.OK, MessageBoxImage.Error);
+                x.ShowYesNoMessageBox = (message, title, button) => MessageBox.Show(message, title, button);
+            };
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVMApp/MessageBoxMVVM/MessageBoxServiceViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Input; // ICommand用
+
+namespace MessageBoxMVVM
+{
+    class MessageBoxServiceViewModel : Microsoft.TeamFoundation.MVVM.ViewModelBase
+    {
+        private ICommand _showCommand;
+        public ICommand ShowCommand
+        {
+            get
+            {
+                if (_showCommand == null)
+                {
+                    _showCommand = new Microsoft.TeamFoundation.MVVM.RelayCommand(ExecuteShowCommand);
+                }
+                return _showCommand;
+            }
+        }
+
+
+        private void ExecuteShowCommand(object x)
+        {
+            // Microsoft.TeamFoundation.MVVM.ViewModelBaseのMessageBoxServiceプロパティに、
+            // MessageBoxに関するものが設定されている
+            var r = MessageBoxService.Show("Yes or No", "title", System.Windows.MessageBoxButton.YesNo);
+
+            if (r == System.Windows.MessageBoxResult.Yes)
+            {
+                MessageBoxService.ShowInformation("Yes", "info");
+            }
+            else
+            {
+                MessageBoxService.ShowError("No", "error");
+            }
+        }
+    }
+}
