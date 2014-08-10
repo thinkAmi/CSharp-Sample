42b54c27614a8dfe158b1092cc5455a8aa3d28e5
thinkAmi <dev.thinkami+git@gmail.com>
2014-08-11 04:25:27 +0900

    add WindowDisplayService sample

---
+++ MVVMApp/ShowWindowMVVM/DataContextChangedParentViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Input; // ICommand用
+
+namespace ShowWindowMVVM
+{
+    public class DataContextChangedParentViewModel : Microsoft.TeamFoundation.MVVM.ViewModelBase
+    {
+        private ICommand _showDialogCommand;
+        public ICommand ShowDialogCommand
+        {
+            get
+            {
+                if (_showDialogCommand == null)
+                {
+                    _showDialogCommand = new Microsoft.TeamFoundation.MVVM.RelayCommand(ExecuteShowDialogCommand);
+                }
+                return _showDialogCommand;
+            }
+        }
+
+
+        private void ExecuteShowDialogCommand(object x)
+        {
+            var r = DataContextChangedChildViewModel.ShowDialog();
+        }
+    }
+}
