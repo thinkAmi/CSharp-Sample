59393d224746636960d3a63aa8242a0c478ebf26
thinkAmi <dev.thinkami+git@gmail.com>
2014-09-06 07:09:33 +0900

    add LoginWindowClose sample

---
+++ MVVMApp/LoginWindowCloseMVVM/LoggedinViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.TeamFoundation.MVVM;    // ViewModelBase
+using System.Runtime.CompilerServices;  // CallerMemberName属性
+
+namespace LoginWindowCloseMVVM
+{
+    public class LoggedinViewModel : ViewModelBase
+    {
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        protected override void RaisePropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            base.RaisePropertyChanged(propertyName);
+        }
+    }
+}
---
+++ MVVMApp/LoginWindowCloseMVVM/LoginEventTrigerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.TeamFoundation.MVVM;    // ViewModelBase
+using System.Windows.Input;             // ICommand
+using System.Security.Principal;        // GenericIdentity, GenericPrincipal
+using System.Threading;                 // Thread
+
+namespace LoginWindowCloseMVVM
+{
+    public class LoginEventTrigerViewModel : ViewModelBase
+    {
+        private ICommand _loginCommand;
+        public ICommand LoginCommand
+        {
+            get
+            {
+                if (_loginCommand == null)
+                {
+                    _loginCommand = new Microsoft.TeamFoundation.MVVM.RelayCommand(ExecuteLoginCommand);
+                }
+                return _loginCommand;
+            }
+        }
+
+        private void ExecuteLoginCommand(object x)
+        {
+            var identity = new GenericIdentity("EventTriger");
+            var principal = new GenericPrincipal(identity, new string[] { "fuga" });
+            Thread.CurrentPrincipal = principal;
+        }
+    }
+}
