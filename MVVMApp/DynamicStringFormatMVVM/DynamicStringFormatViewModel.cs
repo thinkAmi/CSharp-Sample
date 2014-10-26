65f337bab92f12e97c562ed73fc0bf1273e02551
thinkAmi <dev.thinkami+git@gmail.com>
2014-10-27 06:25:18 +0900

    add DynamicStringFormat sample

---
+++ MVVMApp/DynamicStringFormatMVVM/DynamicStringFormatViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.TeamFoundation.MVVM;
+
+namespace DynamicStringFormatMVVM
+{
+    public class DynamicStringFormatViewModel : ViewModelBase
+    {
+        public int Quantity { get { return 1000000; } }
+        public int MinusQuantity { get { return -1000000; } }
+
+
+        public bool Reverse { get { return true; } }
+        public bool Normal { get { return false; } }
+    }
+}
---
+++ MVVMApp/DynamicStringFormatMVVM/MultiBindingConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicStringFormatMVVM
+{
+    public class MultiBindingConverter : System.Windows.Data.IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            // MultiBindingを使うと、第1引数で配列として設定した順に渡される
+            var isReverse = values[0] == null ? false : (bool)values[0];
+            var displayValue = values[1] == null ? 0 : (int)values[1];
+
+            var format = isReverse ? "-##,#;##,#" : "##,#";
+            return string.Format("{0:" + format + "}", displayValue);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
